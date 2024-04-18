174990f8e2f3633882bad3bd2fc950d9bb7a6b87
DinDenz <DinDenz@users.noreply.github.com>
2024-04-18 18:26:06 +0300

    add postSearch

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,38 +1,28 @@
-import { Metadata } from "next";
-import Link from "next/link";
-
-//данные кэшируются ПУ - т е при перезагрузке не будет происходить повторный запрос
-//мы сами можем настроить частоту повторных запросов, тут через 60 секунд
-async function getData() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
-        next: {
-            revalidate: 60
-        }
-    });
-
-    if (!res.ok) throw new Error('Unable to fetch posts')
-
-    return res.json()
-}
+"use client";
+import { getAllPosts } from "@/services/getPosts";
+import { useEffect, useState } from "react";
+import { Posts } from "../components/Posts";
+import { PostSearch } from "../components/PostSearch";
 
 
-export const metadata: Metadata = {
-    title: 'Blog',
-  }
+export default function Blog () {
+    const [posts, setPosts] = useState<any[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
 
-export default async function Blog () {
-const posts = await getData();
+    useEffect(()=> {
+        getAllPosts()
+        .then(setPosts)
+        .finally(() => setLoading(false))
+    },[])
+
 
     return (
        <>
-           <h1>Blog page</h1> 
-           <ul>
-            {posts.map( (post : any) => (
-                <li key={post.id}>
-                    <Link href={`/blog/${post.id}`}>{post.title}</Link>
-                </li>
-           ))}
-           </ul>
+       <h1>Blog page</h1> 
+       <PostSearch onSearch={setPosts}/>
+       {loading? (<h3>Loading...</h3>) : (
+           <Posts posts={posts}/>
+       )}        
        </>
        
     ) 
---
+++ services/getPosts.ts
@@ -0,0 +1,14 @@
+export const getAllPosts = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+    if (!res.ok) throw new Error('Unable to fetch posts')
+
+    return res.json()
+}
+
+export const getPostsBySearch = async (search:string) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?q=${search}`);
+    if (!res.ok) throw new Error('Unable to fetch posts')
+
+    return res.json()
+}
