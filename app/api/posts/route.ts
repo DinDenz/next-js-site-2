6436185880f022e394e03b345b52d1a5e38c8883
DinDenz <DinDenz@users.noreply.github.com>
2024-04-19 19:45:42 +0300

    using search api

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,8 +3,11 @@ import  { posts} from './posts';
 
 export async function GET(req: Request) {
     const {searchParams} = new URL(req.url);
+    
     const query = searchParams.get('q');
+
     let currentPosts = posts;
+
     if(query) {
         currentPosts = posts.filter(post => post.title.toLowerCase().includes(query.toLowerCase()))
     }
